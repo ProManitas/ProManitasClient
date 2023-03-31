6eac94f104bc7e40081b28d01ce747beccce56c8
lucio <luciocucchiarelli_01@hotmail.com>
2023-03-31 15:54:19 -0300

    add gitiHome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import Home from "./Views/Home/Home.jsx"
+// import Detail from 
+import  LoginForm from "./Views/Login/LoginForm/LoginForm.jsx";
+import RegistryForm from "./Views/Login/RegistryForm/RegistryForm.jsx";
+import About from "./Components/Footer/About/About.js"
+import FooterForm from "./Components/Footer/FooterForm/FooterForm.js"
 import { Route, Routes } from "react-router-dom"
 
 
 function App() {
   return (
-    
-      <div>
-        <Routes>
-          <Route exact path="/home" element={<Home/>} />
-        </Routes>
-      </div>
+
+    <div>
+      <Routes>
+        {/* <Route path="/detail" element={<Detail />} /> */}
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/registryForm" element={<RegistryForm />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route path ="/about" element ={<About />}/>
+        <Route path ="/contact" element={< FooterForm/>}  />
+        {/* <Route exact path = "/" element={<Landing/>}/> */}
+      </Routes>
+    </div>
   );
 }
 
