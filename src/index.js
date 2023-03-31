bda593f19150f933240c021bd46ae10f3ae9026c
lucio <luciocucchiarelli_01@hotmail.com>
2023-03-31 10:49:15 -0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"
 
+// import "/.index.css"; 
+import App from "./App";
+import { Provider } from "react-redux"; // orovider para que redux reconozca lo que tenemos dentro 
+import { store } from "./Redux/Store/index"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+  </Provider>,
+
+  document.getElementById('root')
 );
 
 
