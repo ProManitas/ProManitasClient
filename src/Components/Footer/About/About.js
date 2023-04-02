2fd2e1e8b648190501a93a5f706fa1045fbad5ca
yanirc1981 <105172384+yanirc1981@users.noreply.github.com>
2023-04-01 22:44:49 -0300

    formPosteoListo

--- src/Components/Footer/About/About.js
+++ src/Components/Footer/About/About.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import * as React from "react";
+import Button from "@mui/material/Button";
+//import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+//import Toolbar from '@mui/material/Toolbar';
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+//import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8];
 
+const theme = createTheme();
+export default function About() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
 
-const About = () =>{
-    return (
-        <div><h1>QUIENES SOMOS</h1></div>
-    )
-};
-
-export default About;
+      <Container sx={{ py: 8 }} maxWidth="md">
+        {/* End hero unit */}
+        <Grid container spacing={16}>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  sx={{
+                    // 16:9
+                    pt: "56.25%",
+                  }}
+                  image="https://assets.soyhenry.com/LOGO-REDES-01_og.jpg"
+                  alt="random"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Alumno x
+                  </Typography>
+                  <Typography>Desarrollador FullStack</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">In</Button>
+                  <Button size="small">GitHub</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  );
+}
