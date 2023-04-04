7997b15cb3aedf735caaab4133886b2f1f06dcae
yanirc1981 <105172384+yanirc1981@users.noreply.github.com>
2023-04-04 10:31:17 -0300

    add reducer

--- src/Redux/Store/index.js
+++ src/Redux/Store/index.js
@@ -1,6 +1,17 @@
 import {createStore, applyMiddleware } from "redux"; 
 import {composeWithDevTools} from "redux-devtools-extension"; 
 import thunk from "redux-thunk";
-import rootReducer from "../Reducer/index"; 
+import rootReducer from "../Reducer/homeReducer"; 
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+
+/*import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+import  reducer  from  ".. /reducer/index";
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;*/ 
