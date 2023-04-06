14cee4d232627ab9865d4311cef2d269b1901193
lucio <luciocucchiarelli_01@hotmail.com>
2023-04-06 18:31:44 -0300

    actualizacion_paginado

--- src/Redux/Reducer/index.js
+++ src/Redux/Reducer/index.js
@@ -2,13 +2,15 @@ import { combineReducers } from "redux";
 import homeReducer from "./homeReducer";
 import userReducer from "../Reducer/userReducer"
 import detailReducer from "../Reducer/detailReducer"
+import pageReducer from "./paginadoReducer";
 //aca importamos todos los reducers
 
 
 const reducer= combineReducers({
     home: homeReducer,
     user: userReducer,
-    detail: detailReducer
+    detail: detailReducer,
+    paginado: pageReducer
 });
 //aca  traemos dentro de comnineReducer todos los reducers, ejemplo:
 // user:userReducers
