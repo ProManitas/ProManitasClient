29133f02a50c80da4d6ba0f335358f350879c1a5
Julian Riera <105263588+Srezequielr@users.noreply.github.com>
2023-04-20 11:05:15 -0300

    Detail stetic and redux detail solved

--- src/Redux/Actions/detailAction.js
+++ src/Redux/Actions/detailAction.js
@@ -6,21 +6,21 @@ export const FETCH_DETAIL_FAILURE = "FETCH_DETAIL_FAILURE";
 
 export const fetchDetailRequest = () => {
   return {
-    type: FETCH_DETAIL_REQUEST
+    type: FETCH_DETAIL_REQUEST,
   };
 };
 
 export const fetchDetailSuccess = (data) => {
   return {
     type: FETCH_DETAIL_SUCCESS,
-    payload: data
+    payload: data,
   };
 };
 
 export const fetchDetailFailure = (error) => {
   return {
     type: FETCH_DETAIL_FAILURE,
-    payload: error
+    payload: error,
   };
 };
 
@@ -31,7 +31,9 @@ export const getDetail = (postId) => {
       const response = await axios.get(`/adposts/${postId}`);
       const detailData = {
         description: response.data.data.description,
-        image: response.data.data.image
+        image: response.data.data.image,
+        UserId: response.data.data.UserId,
+        ServiceId: response.data.data.ServiceId,
       };
       dispatch(fetchDetailSuccess(detailData));
     } catch (error) {
@@ -86,21 +88,21 @@ export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
 
 export const fetchUserRequest = () => {
   return {
-    type: FETCH_USER_REQUEST
+    type: FETCH_USER_REQUEST,
   };
 };
 
 export const fetchUserSuccess = (data) => {
   return {
     type: FETCH_USER_SUCCESS,
-    payload: data
+    payload: data,
   };
 };
 
 export const fetchUserFailure = (error) => {
   return {
     type: FETCH_USER_FAILURE,
-    payload: error
+    payload: error,
   };
 };
 
@@ -110,7 +112,7 @@ export const getUser = (userId) => {
     try {
       const response = await axios.get(`/users/${userId}`);
       const userData = {
-        image: response.data.data.image
+        image: response.data.data.image,
       };
       dispatch(fetchUserSuccess(userData));
     } catch (error) {
@@ -119,13 +121,86 @@ export const getUser = (userId) => {
   };
 };
 
+export const FETCH_POST_DETAIL_REQUEST = "FETCH_POST_DETAIL_REQUEST";
+export const FETCH_POST_DETAIL_SUCCESS = "FETCH_POST_DETAIL_SUCCESS";
+export const FETCH_POST_DETAIL_FAILURE = "FETCH_POST_DETAIL_FAILURE";
 
+export const fetchPostDetailRequest = () => {
+  return {
+    type: FETCH_SERVICE_DETAIL_REQUEST,
+  };
+};
 
+export const fetchPostDetailSuccess = (data) => {
+  return {
+    type: FETCH_POST_DETAIL_SUCCESS,
+    payload: data,
+  };
+};
 
+export const fetchPostDetailFailure = (error) => {
+  return {
+    type: FETCH_SERVICE_DETAIL_FAILURE,
+    payload: error,
+  };
+};
 
+export const getPostDetail = (id) => {
+  console.log("me estan ejecutano");
+  return async (dispatch) => {
+    dispatch(fetchPostDetailRequest());
+    try {
+      const postDetail = await getPost(id);
+      const userId = postDetail.UserId;
+      const serviceId = postDetail.ServiceId;
+      const userDetail = await getUserDetail(userId);
+      const service = await getService(serviceId);
+      const dataUser = {
+        firstname: userDetail.firstname,
+        lastname: userDetail.lastname,
+        image: userDetail.image,
+        UserId: userId,
+      };
+      const dataPost = {
+        description: postDetail.description,
+        image: postDetail.image,
+        service: service,
+      };
+      const data = {
+        dataPost: dataPost,
+        dataUser: dataUser,
+      };
+      dispatch(fetchPostDetailSuccess(data));
+    } catch (error) {
+      console.log("error");
+      dispatch(fetchPostDetailFailure(error.message));
+    }
+  };
+};
 
-  
+const getPost = async (id) => {
+  try {
+    const jsonPost = await axios.get(`/adposts/${id}`);
+    return jsonPost.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
+const getUserDetail = async (id) => {
+  try {
+    const jsonUser = await axios.get(`/users/${id}`);
+    return jsonUser.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
-
-    
+const getService = async (id) => {
+  try {
+    const jsonService = await axios.get(`/services/${id}`);
+    return jsonService.data.data.name;
+  } catch (error) {
+    throw error;
+  }
+};
