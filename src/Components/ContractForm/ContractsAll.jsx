5cf08ed0805b4e9e21ff8eff56d7142a1bfa8fb1
mlobeto <mercedeslobeto@gmail.com>
2023-04-20 13:12:55 -0300

    Ultimo2

--- src/Components/ContractForm/ContractsAll.jsx
+++ src/Components/ContractForm/ContractsAll.jsx
@@ -5,53 +5,51 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllContracts } from "../../Redux/Actions/contractAction"
 import { getAllUsers } from "../../Redux/Actions/userAction";
 import {Link} from "react-router-dom"
-
+import { Button } from '@mui/material';
 
 const ContractsAll = () => {
 
-const {user} = useAuth0()
-const dispatch = useDispatch()
-
-    //LLENO EL ESTADO CON LOS CONTRATOS Y LOS USUARIOS
-useEffect(()=>{
-dispatch(getAllContracts())
-dispatch(getAllUsers());
-
-}, [dispatch])
-
-  // ME TRAIGO EL ESTADO DE USER Y CONTRACTS
-  const allContracts = useSelector((state) => state.contracts.contracts);
-  const usersDb = useSelector((state) => state.user.allUsers);
-
-//FILTRO EL USUARIO QUE COINCIDA CON EL MAIL QUE ESTA EN SESION
-const filteredUser = usersDb.filter((elem) => elem.email === user.email);
-console.log(filteredUser)
-
-//COMPARO EL USUARIO CON EL USERID DEL CONTRATO PARA OBTENER LA INFO DE ESE CONTRATO
-const filterInfo = allContracts.filter(inf => inf.UserId === filteredUser[0].id)
-console.log(filterInfo)
-
-
-const textDescription =
-"**Con la creación de este contrato, usted acepta y se compromete a cumplir con nuestros términos y condiciones donde se describen las obligaciones y responsabilidades tanto del usuario como de nuestra empresa. ";
-
-const lastContractId = filterInfo.length > 0 ? filterInfo[filterInfo.length - 1].id : null;
-
-return (
-    <div>
+    const { user } = useAuth0();
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(getAllContracts());
+      dispatch(getAllUsers());
+    }, [dispatch]);
+  
+    const allContracts = useSelector((state) => state.contracts.contracts);
+    const usersDb = useSelector((state) => state.user.allUsers);
+  
+    const filteredUser = usersDb.filter((elem) => elem.email === user.email);
+  
+    const filterInfo = allContracts.filter(inf => inf.UserId === filteredUser[0].id);
+  
+    const textDescription =
+      "**Con la creación de este contrato, usted acepta y se compromete a cumplir con nuestros términos y condiciones donde se describen las obligaciones y responsabilidades tanto del usuario como de nuestra empresa. ";
+  
+    const lastContractId = filterInfo.length > 0 ? filterInfo[filterInfo.length - 1].id : null;
+  
+    return (
+      <div>
+        {lastContractId && (
+          <Link to={`/payment/${lastContractId}`}>
+   
+            <Button variant="contained" color="primary">
+              Pagar último contrato
+            </Button>
+          </Link>
+        )}
         {filterInfo?.map(inf => (
-            <ContractPDF 
-                contractId = {inf.id}
-                username = {filteredUser[0].username}
-                description = {textDescription}
-                detail = {inf.detail}
-                payment = {inf.payment}
-            />
+          <ContractPDF 
+            contractId={inf.id}
+            username={filteredUser[0].username}
+            description={textDescription}
+            detail={inf.detail}
+            payment={inf.payment}
+          />
         ))}
-        {/* REDIRIGIR AL PAGO CON EL ID DEL ÚLTIMO CONTRATO GENERADO */}
-        {lastContractId && <Link to={`/payment/${lastContractId}`}><button>Pago</button></Link>}
-    </div>
-);
-}
-
-export default ContractsAll;
+      </div>
+    );
+  }
+  
+  export default ContractsAll;
